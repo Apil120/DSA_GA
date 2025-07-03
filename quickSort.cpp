b06aa46ccd61f7ebce9222cb6100dc58507d3b31
Apil120 <apilisadhikari@gmail.com>
2025-07-04 00:10:46 +0545

    Improve code consistency by adding std:: prefix and enhancing comments in quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
-using namespace std;
+using namespace std::chrono;
 
-int helper(vector<int>& arr, int low, int high) {
+//Helper function for quick sort
+int helper(std::vector<int>& arr, int low, int high) {
     int pivot = arr[high]; 
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
-            swap(arr[i], arr[j]);
+            std::swap(arr[i], arr[j]);
         }
     }
 
-    swap(arr[i + 1], arr[high]);
+    std::swap(arr[i + 1], arr[high]);
     return i + 1;
 }
 
-void quickSort(vector<int>& arr, int low, int high) {
+//Quick Sort
+void quickSort(std::vector<int>& arr, int low, int high) {
     if (low < high) {
         int pivotIndex = helper(arr, low, high);
         quickSort(arr, low, pivotIndex - 1);
@@ -28,20 +31,23 @@ void quickSort(vector<int>& arr, int low, int high) {
 
 int main() {
     int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> n;
 
-    vector<int> arr(n);
-    cout << "Enter " << n << " elements (unsorted): ";
+    std::vector<int> arr(n);
+    std::cout << "Enter " << n << " elements (unsorted): ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        std::cin >> arr[i];
     }
+
+    //Time start
+    
 
     quickSort(arr, 0, n - 1);
 
-    cout << "Sorted array: ";
+    std::cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        std::cout << arr[i] << " ";
     }
 
     return 0;
