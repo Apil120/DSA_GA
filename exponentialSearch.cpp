409939eb4c7beeffb9b245806515bf15132ae0f2
Apil120 <apilisadhikari@gmail.com>
2025-07-03 07:19:47 +0545

    Refactor sorting implementation: replace merge sort with quick sort in exponential search

--- exponentialSearch.cpp
+++ exponentialSearch.cpp
@@ -2,40 +2,26 @@
 #include <vector>
 using namespace std;
 
-void merge(vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+int helper(vector<int>& arr, int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1;
 
-    vector<int> L(n1), R(n2);
-
-    for (int i = 0; i < n1; ++i)
-        L[i] = arr[left + i];
-    for (int i = 0; i < n2; ++i)
-        R[i] = arr[mid + 1 + i];
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j])
-            arr[k++] = L[i++];
-        else
-            arr[k++] = R[j++];
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
     }
 
-    while (i < n1)
-        arr[k++] = L[i++];
-
-    while (j < n2)
-        arr[k++] = R[j++];
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
 }
 
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivotIndex = helper(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
     }
 }
 
@@ -74,8 +60,13 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    mergeSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
 
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
     int target;
     cout << "\nEnter the target element to search: ";
     cin >> target;
